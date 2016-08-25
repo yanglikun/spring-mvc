10b31595407842db54c8719853df6499d695bed7
yanglikun <test@jd.com>
2016-08-25 09:08:32 +0800

    添加SPI的说明

--- src/test/java/spi/SPITest.java
+++ src/test/java/spi/SPITest.java
@@ -6,6 +6,12 @@ import java.util.Comparator;
 import java.util.ServiceLoader;
 
 /**
+ * <ul>
+ * SPI(Service Provider Interface),自动发现接口的实现类
+ * <ol>①在类路径下有目录META-INF/services目录</ol>
+ * <ol>在①中的目录下添加文件，文件名就是 接口的名字</ol>
+ * <ol>文件的内容就是实现类的全限定名，多个用逗号隔开</ol>
+ * </ul>
  * Created by yanglikun on 2016/8/25.
  */
 public class SPITest {
